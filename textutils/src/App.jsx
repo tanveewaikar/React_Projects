52ff3134c773e10b90f78a86714bbf453030246c
Tanvee Mohan Waikar <tanveewaikar1311@gmail.com>
2025-11-29 02:26:07 +0530

    Added navbar to TextUtils

--- textutils/src/App.jsx
+++ textutils/src/App.jsx
@@ -7,17 +7,28 @@ function App() {
   let name = "Tanvee";
   return (
     <>
-      <nav>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </nav>
-      <img src="" alt="" />
-      <div>
-        <h1>Name {name}</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum a culpa tempore necessitatibus pariatur adipisci 
-           consequuntur alias, dicta repudiandae vel quo placeat autem? Eveniet molestias dolores numquam rem laudantium rerum natus id illo commodi eligendi nihil quisquam hic, officiis ab accusantium minima molestiae asperiores, laboriosam maiores est modi harum dignissimos!</p>
-      </div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">About</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
     </>
   )
 }

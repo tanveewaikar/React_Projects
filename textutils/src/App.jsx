fbb87ee89d572f20d5c72a20fdca193bdf1ecdd7
Tanvee Mohan Waikar <tanveewaikar1311@gmail.com>
2025-12-21 20:48:06 +0530

    About us page added

--- textutils/src/App.jsx
+++ textutils/src/App.jsx
@@ -4,14 +4,16 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import TextForm from "./components/TextForm";
+import AboutUs from "./components/AboutUs";
 
 function App() {
   return (
     <>
     <NavBar title ="TextUtils" aboutText="About Us" />
-    <div className="container">
+    {/* <div className="container">
       <TextForm heading ="Enter a text to analyze below"/>
-    </div>
+    </div> */}
+    <AboutUs/>
     </>
   )
 }
